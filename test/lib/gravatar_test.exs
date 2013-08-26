3b7261aba445e32dbe897030100259fcfc1c8ead
Tom Janssens <Tom@corebvba.be>
2013-08-26 10:34:54 +0200

    Implement a testcase to get gravatar info from an email address.

--- test/lib/gravatar_test.exs
+++ test/lib/gravatar_test.exs
@@ -12,4 +12,10 @@ defmodule GravatarTest do
   		"http://www.gravatar.com/df6affd04f619165417f87f07028f068.xml"
   end
 
+  test "return a user profile for an email address" do
+  	assert Gravatar.email_to_profile("tom@corebvba.be") ==
+  		[ display_name: "Tom Janssens",
+ 		  avatar_url: "http://1.gravatar.com/avatar/df6affd04f619165417f87f07028f068",
+ 		  email: "tom@corebvba.be"]
+  end
 end
